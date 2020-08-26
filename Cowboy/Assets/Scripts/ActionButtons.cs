f6a2a3652e45ec50a4166b347b1f2566cb1c0742
Lexutor <49167230+Lexutor@users.noreply.github.com>
2020-08-26 01:47:14 -0400

    Creación de botones de acción
    Los botones de acción son creados, por el momento no tienen acción alguna y solo se quedan en la pantalla (el texto que tienen es temporal). La hoja de código también está creada y solo tiene algunas funciones vacías. Los botones también se escalan y posicionan automáticamente.

---
+++ Cowboy/Assets/Scripts/ActionButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButtons : MonoBehaviour
+{
+    private void DodgeLeft()
+    {
+        //
+    }
+
+    private void DodgeRight()
+    {
+        //
+    }
+
+    private void Shoot()
+    {
+        //
+    }
+}
