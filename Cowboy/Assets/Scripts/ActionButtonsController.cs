7a0e6d0a071ccefe0c688d066e629a62a6264032
Lexutor <49167230+Lexutor@users.noreply.github.com>
2020-08-26 23:21:34 -0400

    Activación y desactivación de los botones de acción
    Ahora los botones de acción pueden activarse o desactivarse para que se usen solo cuando sean necesarios. Por ahora basta con cambiar el valor de la variable "takeAction".

---
+++ Cowboy/Assets/Scripts/ActionButtonsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButtonsController : MonoBehaviour
+{
+    public GameObject[] actionButtons; //Lista con los botones
+
+    public bool takeAction = false;
+
+    void LateUpdate()
+    {
+        if (takeAction)
+        {
+            SetActionButtons(true);
+        }
+        else
+        {
+            SetActionButtons(false);
+        }
+    }
+
+    /// <summary>
+    /// Activa o desactiva los botones de acción
+    /// </summary>
+    /// <param name="state">Nuevo estado de los botones de acción.</param>
+    private void SetActionButtons(bool state)
+    {
+        foreach (GameObject button in actionButtons)
+        {
+            button.gameObject.SetActive(state);
+        }
+    }
+}
--- Cowboy/Assets/Scripts/PlayerController.cs
+++ Cowboy/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("References")]
     public Camera playerCamera;
+    public ActionButtonsController actionButtonsController;
 
     [Header("PlayerStads")]
     public float dashSpeed = 200f;
@@ -17,19 +18,32 @@ public class PlayerController : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
+    /// <summary>
+    /// El personaje esquiva hacia la izquierda.
+    /// </summary>
     public void DodgeLeft()
     {
         playerRigidbody.velocity = Vector3.left * dashSpeed * Time.deltaTime;
+        actionButtonsController.takeAction = false;
+        //Luego de realizar la acción, se ocultan los botones
     }
 
+    /// <summary>
+    /// El personaje esquiva hacia la derecha.
+    /// </summary>
     public void DodgeRight()
     {
         playerRigidbody.velocity = Vector3.right * dashSpeed * Time.deltaTime;
+        actionButtonsController.takeAction = false;
     }
 
+    /// <summary>
+    /// El personaje dispara su arma.
+    /// </summary>
     public void Shoot()
     {
         Debug.Log("Disparo");
+        actionButtonsController.takeAction = false;
     }
 
 }
